5306bc3366c422d70aca6795d4a413e0a70db11b
kadowaki-dev <kadowakikaoru@87.115.238.10.ty1.coins.jp.ricoh.com>
2020-04-13 14:24:25 +0900

    APEPCMACTT-1135  カメラの位置/向きを変更

--- index.js
+++ index.js
@@ -45,20 +45,51 @@ function init() {
   // シーンにライト追加
   scene.add(light);
 
+  let rot=0
+  let mouseX=0
+
   // 初回実行
   tick();
+  // requestAnimationFrame(tick);  //1回だけ動かす用
 
   function tick() {
     requestAnimationFrame(tick);
 
     // 箱を回転させる
-    box.rotation.x += 0.1; //x軸の回転角 (ラジアン)
-    box.rotation.y += 0.1; //y軸の回転角 (ラジアン)
-    box.rotation.z += 0.1; //z軸の回転角 (ラジアン)
+    // box.rotation.x += 0.1; //x軸の回転角 (ラジアン)
+    // box.rotation.y += 0.1; //y軸の回転角 (ラジアン)
+    // box.rotation.z += 0.1; //z軸の回転角 (ラジアン)
+
+    //自動回転 度数
+    //rot+=0.5
+
+    // マウスの位置に応じて角度を設定
+    // マウスのX座標がステージの幅の何%の位置にあるか調べてそれを360度で乗算する
+    const targetRot = (mouseX / window.innerWidth) * 360;
+    // イージングの公式を用いて滑らかにする
+    // 値 += (目標値 - 現在の値) * 減速値
+    rot += (targetRot - rot) * 0.02;
+    
+    // ラジアンに変換する
+    const radian = rot * Math.PI / 180; 
+
+    // 角度に応じてカメラの位置を設定
+    camera.position.x = 1000 * Math.sin(radian);
+    camera.position.z = 1000 * Math.cos(radian);
+
+    // 原点方向を見つめる
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    // ライトも動かす（ライトの向きは？）
+    light.position.x = 1000 * Math.sin(radian);
+    light.position.z = 1000 * Math.cos(radian);
 
     // レンダリング
     renderer.render(scene, camera);
   }
 
-
+  // マウス座標はマウスが動いた時のみ取得できる
+  document.addEventListener("mousemove", (event) => {
+    mouseX = event.pageX;
+  });
 }
